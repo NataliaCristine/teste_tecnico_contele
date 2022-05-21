7feadc8791e5b7db3a2429d280d86195b3c6c36d
Natalia Nunes <naticristinenunes@gmail.com>
2022-05-21 20:18:46 -0300

    fix: configurado docker e docker-compose up

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 const knexfile = {
   client: "postgresql",
   connection: {
-    database: "", // trocar para credenciais do docker compose
-    user: "",
-    password: "",
+    database: "test", // trocar para credenciais do docker compose
+    user: "test",
+    password: "test",
+    port: 5431,
+    host: process.env.DATABASE_HOST,
   },
   pool: {
     min: 2,
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { initRouter } from "./routes/index.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+// import swaggerUiExpress from "swagger-ui-express";
+// import swaggerDocument from "../swagger.json";
 
 const app = express();
 
@@ -9,6 +11,12 @@ app.use(express.json());
 initRouter(app);
 
 app.use(errorHandler);
+
+// app.use(
+//   "/api-documentation",
+//   swaggerUiExpress.serve,
+//   swaggerUiExpress.setup(swaggerDocument)
+// );
 
 app.listen(3000, () => {
   console.log("App rodando na porta: http://localhost:3000");
